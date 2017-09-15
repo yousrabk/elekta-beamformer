c615ea841e5aa06011ef85ec50a2f2f80f105705
Yousra Bekhti <yousra.bekhti@gmail.com>
2017-09-15 13:38:44 +0200

    running script for mxne

--- mne/analyze_phantom_mixed_norms.py
+++ mne/analyze_phantom_mixed_norms.py
@@ -1,47 +1,65 @@
 # -*- coding: utf-8 -*-
 """
 =============================
-Elekta phantom data with MxNE
+Elekta phantom data with LCMV
 =============================
 """
+# authors: Amit & Alex & Eric
 
 from __future__ import print_function
 
-import numpy as np
+from itertools import product
 
-from phantom_helpers import (get_data, get_fwd, plot_errors, actual_pos,
-                             maxfilter_options, dipole_indices,
-                             dipole_amplitudes)
+import numpy as np
+import pandas as pd
+# import mne
+from mne.parallel import parallel_func
 from mne.inverse_sparse import mixed_norm
 
-errors = np.empty(
-    (len(maxfilter_options), len(dipole_amplitudes), len(dipole_indices)))
+from phantom_helpers import get_data, plot_errors, get_bench_params, get_fwd
+from phantom_helpers import get_dataset
 
-src, fwd = get_fwd()
-loose, depth = None, 0.9
+# base_path, postfix = get_dataset('aston')
+base_path, postfix = get_dataset('')
 
-for ui, use_maxwell_filter in enumerate(maxfilter_options):
-    for ai, dipole_amplitude in enumerate(dipole_amplitudes):
-        print(('Processing : %4d nAm : SSS=%s'
-               % (dipole_amplitude, use_maxwell_filter)).ljust(40), end='')
-        for di, dipole_idx in enumerate(dipole_indices):
-            epochs, evoked, cov, sphere = \
-                get_data(dipole_idx, dipole_amplitude, use_maxwell_filter)
-            stc = mixed_norm(evoked, fwd, cov, alpha=60, n_mxne_iter=10,
-                             loose=loose, depth=depth, return_residual=False)
-            print('number of sources found: %s lh - %s rh'
-                  % (len(stc.vertices[0]), len(stc.vertices[1])))
-            # Find the best idx
-            idx_max = np.argmax(np.mean(stc.data, axis=1))
-            if idx_max < len(stc.vertices[0]):
-                vertno_max = stc.vertices[0][idx_max]
-            else:
-                vertno_max = stc.vertices[1][idx_max - len(stc.vertices[0])]
-            pos = fwd['src'][0]['rr'][vertno_max]
-            # pos = fwd['src'][0]['rr'][np.where(src[0]['vertno'] ==
-            #                                    stc.vertices[0][0])[0][0]]
-            errors[ui, ai, di] = 1e3 * np.linalg.norm(
-                pos - actual_pos[dipole_idx - 1])
-        print(np.round(errors[ui, ai], 1))
+maxfilter_options, dipole_amplitudes, dipole_indices, actual_pos, bads =\
+    get_bench_params(base_path)
 
-plot_errors(errors, 'MxNE')
+src, fwd = get_fwd(base_path)
+
+columns = ['dipole_index', 'dipole_amplitude', 'maxfilter', 'error']
+loose, depth = 1., 0.9
+
+
+def run(da, di, mf):
+    print(('Processing : %4d nAm (dip %d) : SSS=%s'
+          % (da, di, mf)).ljust(42), end='')
+    epochs, evoked, cov, sphere = get_data(
+        base_path, di, da, mf, bads=bads)
+    # Do MxNE
+    dip = mixed_norm(
+        evoked, fwd, cov, alpha=60., n_mxne_iter=1,
+        depth=depth, return_residual=False, return_as_dipoles=True)
+
+    print(" n_sources=%s" % len(dip), end='')
+    # If stc
+    # idx_max = np.argmax(np.mean(stc.data, axis=1))
+    # vertno_max = stc.vertices[idx_max]
+    # pos = src[0]['rr'][vertno_max]
+
+    # If dipoles
+    amp_max = [np.max(d.amplitude) for d in dip]
+    idx_max = np.argmax(amp_max)
+    pos = dip[idx_max].pos[0]
+    error = 1e3 * np.linalg.norm(pos - actual_pos[di - 1])
+
+    print(" Error=%s mm" % np.round(error, 1))
+    return pd.DataFrame([(di, da, mf, error)], columns=columns)
+
+parallel, prun, _ = parallel_func(run, n_jobs=1)
+errors = parallel([prun(da, di, mf) for mf, da, di in
+                   product(maxfilter_options, dipole_amplitudes,
+                           dipole_indices)])
+errors = pd.concat(errors, axis=0, ignore_index=True)
+
+plot_errors(errors, 'MxNE', postfix=postfix)
